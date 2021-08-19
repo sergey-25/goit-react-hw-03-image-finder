2943ad7b0fca1bac94157c6fe2e3a9b962959557
Kurylenko Serhii <sergey25111988@gmail.com>
2021-08-19 15:25:48 +0300

    hm3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
+import { Component } from "react";
+import { fetchImages } from "./services/pixabayApi";
+import toast, { Toaster } from "react-hot-toast";
+import { Spinner } from "./components/Loader/Loader";
+import { Modal } from "./components/Modal/Modal";
+import { Searchbar } from "./components/Searchbar/Searchbar";
+import  {ImageGallery}  from "./components/ImageGallery/ImageGallery";
+import { Button } from "./components/Button/Button";
+import { Notification } from "./components/Notification/Notification";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react'
+const Status = {
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected",
+};
 
-function App() {
-  return (
-    <div>
-      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
+export default class App extends Component {
+  state = {
+    searchQuery: "",
+    page: 1,
+    images: [],
+    status: Status.IDLE,
+    activeImage: null,
+    error: null,
+  };
 
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-  
-  <div class="offcanvas-body">
-          <input data-bs-toggle="offcanvas"></input>
-          <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-  Button with data-bs-target
-</button>
+  componentDidUpdate(prevProps, prevState) {
+    const { page, searchQuery } = this.state;
 
-  </div>
-</div>
-    </div>
-  )
+    if (prevState.searchQuery !== searchQuery) {
+      this.setState({ images: [] });
+      this.fetchImagesByQuery(searchQuery, page);
+      this.setState({ status: Status.PENDING });
+    }
+
+    if (prevState.page !== page && page !== 1) {
+      this.fetchImagesByQuery(searchQuery, page);
+    }
+
+    if (page > 1) {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }
+
+  fetchImagesByQuery = (searchQuery, page) => {
+    fetchImages(searchQuery, page)
+      .then(({ hits }) => {
+        if (hits.length === 0) {
+          this.setState({ status: Status.IDLE });
+          toast.error("Sorry, we couldn't find any matches");
+        } else {
+          this.setState(({ images }) => ({
+            images: [...images, ...hits],
+            status: Status.RESOLVED,
+          }));
+        }
+      })
+      .catch((error) => {
+        this.setState({ error, status: Status.REJECTED });
+      });
+  };
+
+  handleFormSubmit = (value) => {
+    this.setState({ searchQuery: value, page: 1 });
+  };
+
+  handleButtonClick = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  resetActiveImage = () => {
+    this.setState({ activeImage: null });
+  };
+
+  handleImageClick = (activeImage) => {
+    this.setState({ activeImage });
+  };
+
+  render() {
+    const { images, status, activeImage, error } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} toast={toast} />
+        {status === Status.IDLE && <></>}
+        {status === Status.PENDING && <Spinner />}
+        {status === Status.REJECTED && <Notification text={error} />}
+        {status === Status.RESOLVED && (
+          <>
+            <ImageGallery
+              images={images}
+              onImageClick={this.handleImageClick}
+            />
+            {}
+            <Button onClick={this.handleButtonClick} />
+            {activeImage && (
+              <Modal onClose={this.resetActiveImage}>
+                <img src={activeImage.largeImageURL} alt={activeImage.tags} />
+              </Modal>
+            )}
+          </>
+        )}
+        <Toaster
+          position="top-right"
+          containerClassName=""
+          containerStyle={{ top: 100 }}
+          toastOptions={{
+            style: {
+              borderRadius: "4px",
+              background: "#BA55D3",
+              color: "#fff",
+            },
+          }}
+        />
+      </>
+    );
+  }
 }
-
-export default App
